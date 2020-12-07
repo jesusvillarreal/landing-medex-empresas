8a77096c8ce88493dca764d189eb108ea37f2df7
Jesus Villarreal <jesusvillarrealzavaleta@gmail.com>
2020-12-07 18:32:51 -0500

    Se hizo todos los cambios solicitados. Lunes (Feriado)

---
+++ Scripts/script.js
@@ -0,0 +1,30 @@
+
+const buttonOpen = document.getElementById('open-modal')
+const buttonClose = document.getElementById('close-modal')
+const modal = document.getElementById('modal')
+const toggleActiveModal = () => modal.classList.toggle('active')
+
+buttonClose.addEventListener('click', toggleActiveModal)
+buttonOpen.addEventListener('click', toggleActiveModal)
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
